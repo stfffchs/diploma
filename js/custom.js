99fff46a18b445f9f1141e44b8df686fbc40c826
stfffchs <st.a.fuchs@gmail.com>
2020-05-29 16:39:31 +0200

    add nav pages

--- js/custom.js
+++ js/custom.js
@@ -9,16 +9,16 @@
 
 new WOW().init();
 
-/**== loader js ==**/
+// /**== loader js ==**/
 
-$("body").prepend('<div id="preloader"><div class="spinner-sm spinner-sm-1" id="status"> </div></div>');
-$(window).on('load', function() { // makes sure the whole site is loaded 
-    $('#status').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(350).css({
-        'overflow': 'visible'
-    });
-})
+// $("body").prepend('<div id="preloader"><div class="spinner-sm spinner-sm-1" id="status"> </div></div>');
+// $(window).on('load', function() { // makes sure the whole site is loaded 
+//     $('#status').fadeOut(); // will first fade out the loading animation 
+//     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+//     $('body').delay(350).css({
+//         'overflow': 'visible'
+//     });
+// })
 
 /**== mega menu ==**/
 
